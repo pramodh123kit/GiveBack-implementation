02c22e14e8cd38167b6dc08a143d81e352dfc173
IsinduNimandith23 <isindunimandith23@gmail.com>
2024-02-11 16:15:44 +0530

    Updated the Home Page

--- giveback-frontend-manager/src/pages/Home/Home.jsx
+++ giveback-frontend-manager/src/pages/Home/Home.jsx
@@ -55,6 +55,15 @@ const Home = () => {
             </p>
           </div>
       </div>
+        <div className={styles.fourth_section}>
+        <div className={styles.line1}>
+        <h1 className={styles.first_line}>Are you an Organization looking for help?</h1>
+        <h2 className={styles.second_line}>Join us now!</h2>
+        <img className={styles.arrow} src="../src/Assets/arrow.png" alt="arrow" /> <br />
+        <button className={styles.sign_up_btn}>SIGN UP</button>
+        </div>
+        <img className={styles.line2} src="../src/Assets/Project_70-01 1.png" alt="" />
+      </div>
     </div>
   );
 };
