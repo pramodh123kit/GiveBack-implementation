d360e89375b89a16a74ddf8e749a8931d2cb3665
Pramodh123kit <pramodh123kit@gmail.com>
2024-02-26 10:14:04 +0530

    Added tailwind to the to the Footer

--- giveback-frontend-manager/src/components/common-components/Footer/Footer.jsx
+++ giveback-frontend-manager/src/components/common-components/Footer/Footer.jsx
@@ -1,51 +1,47 @@
 import logoDark from "../../../Assets/logoDark.png";
 import emailIcon from "../../../Assets/email-icon.png";
 import "./Footer.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export const Footer = () => {
   return (
-    <div className="footer__container">
-    <div className="footer__links">
-      <div className="footer__links--wrapper">
-      <div className="footer__link--items">
-      <a href="/contact" id="footer__logo">
-            <img
-              src={logoDark}
-              className="logo"
-              alt="Logo"
-            />
-      </a>
-      </div>
-        <div className="footer__link--items">
-          <h2>Menu</h2>
-          <a href="/home">Home</a>
-          <a href="/donations">Donations</a>
-          <a href="/community">Community Group</a>
-          <a href="/contact">Contact</a>
-          <a href="/about">About</a>
-        </div>
-        <div className="footer__link--items">
-          <h2>Resources</h2>
-          <a href="/MeetTheTeam">Meet The Team</a>
-          <a href="/">FAQ</a>
-          <a href="/">Terms of Service</a>
-        </div>
-      </div>
-      <div className="footer__links--wrapper">
-        <div className="footer__link--items">
-          <h2>Contact</h2>
-          
-          <a
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="footer__container  text-light py-4">
+      <div className="container">
+        <div className="d-flex flex-wrap justify-content-between">
+          <div className="footer__link--items mb-4">
+            <a href="/contact" id="footer__logo" className="d-flex align-items-center">
+              <img src={logoDark} className="logo" alt="Logo" />
+            </a>
+          </div>
+          <div className="footer__link--items mb-4">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-2">Menu</h2>
+            <a href="/home">Home</a>
+            <a href="/donations">Donations</a>
+            <a href="/community">Community Group</a>
+            <a href="/contact">Contact</a>
+            <a href="/about">About</a>
+          </div>
+          <div className="footer__link--items mb-4">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-2">Resources</h2>
+            <a href="/MeetTheTeam">Meet The Team</a>
+            <a href="/">FAQ</a>
+            <a href="/">Terms of Service</a>
+          </div>
+          <div className="footer__link--items">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-2">Contact</h2>
+            <a
+              href="mailto:infoTest@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="d-flex align-items-center"
             >
-            <img src={emailIcon} className="email-icon" alt="Email Icon"/>
-            infoTest@gmail.com</a
-          >
+              <img src={emailIcon} className="email-icon w-25 h-auto mr-2" alt="Email Icon" />
+              infoTest@gmail.com
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
-}
+};
--- giveback-frontend-manager/src/pages/Meet-The-Team/MeetTheTeam.jsx
+++ giveback-frontend-manager/src/pages/Meet-The-Team/MeetTheTeam.jsx
@@ -4,39 +4,36 @@ import icon from '../../Assets/icon.png';
 
 const MeetTheTeam = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.meet_container}>
-      <div className={styles.heading}>
-        <h1>Meet The Team</h1>
+    <div className={`${styles.container} mx-auto w-80`}>
+      <div className={`${styles.meet_container} bg-green-200 rounded-lg shadow-md mb-16`}>
+        <div className={`${styles.heading} font-bold text-center text-2xl my-8 pt-12`}>
+          <h1>Meet The Team</h1>
+        </div>
+        <div className={`${styles.card_container} flex justify-center items-center flex-wrap gap-8`}>
+          <div className={`${styles.card} shadow-md w-64 h-96 bg-white rounded-md mb-16`}>
+            <img className={`${styles.prof_pic} rounded-t-md w-64 h-32`} src={icon} alt="Profile Picture" />
+            <Card name="Pramodh Lihinikaduwa" role="Team Leader" email="pramodh.20220414@iit.ac.lk" github="https://github.com/pramodh123kit" linkedin="https://www.linkedin.com/in/pramodh-lihinikaduwa/" />
+          </div>
+          <div className={`${styles.card} shadow-md w-64 h-96 bg-white rounded-md mb-16`}>
+            <img className={`${styles.prof_pic} rounded-t-md w-64 h-32`} src={icon} alt="Profile Picture" />
+            <Card name="Isindu Nimandith" role="Developer" email="nimandith.20220413@iit.ac.lk" linkedin="https://www.linkedin.com/in/isindu-nimandith-432228299/" github="https://github.com/IsinduNimandith23" />
+          </div>
+          <div className={`${styles.card} shadow-md w-64 h-96 bg-white rounded-md mb-16`}>
+            <img className={`${styles.prof_pic} rounded-t-md w-64 h-32`} src={icon} alt="Profile Picture" />
+            <Card name="Tharusha Chamikara" role="Developer" email="tharusha.20221339@iit.ac.lk" linkedin="https://www.linkedin.com/in/tharusha-chamikara-158798256/" github="https://github.com/chamikarapasquel" />
+          </div>
+          <div className={`${styles.card} shadow-md w-64 h-96 bg-white rounded-md mb-16`}>
+            <img className={`${styles.prof_pic} rounded-t-md w-64 h-32`} src={icon} alt="Profile Picture" />
+            <Card name="Chethana Gayanath" role="Developer" email="chethana.20210674@iit.ac.lk" linkedin="https://www.linkedin.com/in/chethana-gayanath-843719298/" github="https://github.com/chethana87" />
+          </div>
+          <div className={`${styles.card} shadow-md w-64 h-96 bg-white rounded-md mb-16`}>
+            <img className={`${styles.prof_pic} rounded-t-md w-64 h-32`} src={icon} alt="Profile Picture" />
+            <Card name="Akash Hewavickrama" role="Developer" email="chandrasekara.20222237@iit.ac.lk" linkedin="https://www.linkedin.com/in/rajeev-hewavickrama-5297bb266/" github="https://github.com/RajeevAkash" />
+          </div>
+        </div>
       </div>
-      <div className={styles.card_container}>
-        <div className={styles.card}>
-          <img className={styles.prof_pic} src={icon}></img>
-          <Card name="Pramodh Lihinikaduwa" role="Team Leader" email="pramodh.20220414@iit.ac.lk" github="https://github.com/pramodh123kit" linkedin="https://www.linkedin.com/in/pramodh-lihinikaduwa/" />
-        </div>
-        <div className={styles.card}>
-          <img className={styles.prof_pic} src={icon}></img>
-          <Card name="Isindu Nimandith" role="Developer" email="nimandith.20220413@iit.ac.lk"  linkedin="https://www.linkedin.com/in/isindu-nimandith-432228299/" github="https://github.com/IsinduNimandith23" />
-        </div>
-        <div className={styles.card}>
-          <img className={styles.prof_pic} src={icon}></img>
-          <Card name="Tharusha Chamikara" role="Developer" email="tharusha.20221339@iit.ac.lk" linkedin="https://www.linkedin.com/in/tharusha-chamikara-158798256/" github="https://github.com/chamikarapasquel" />
-        </div>
-        <div className={styles.card}>
-          <img className={styles.prof_pic} src={icon}></img>
-          <Card name="Chethana Gayanath" role="Developer" email="chethana.20210674@iit.ac.lk" linkedin="https://www.linkedin.com/in/chethana-gayanath-843719298/" github="https://github.com/chethana87" />
-        </div>
-        <div className={styles.card}>
-          <img className={styles.prof_pic} src={icon}></img>
-          <Card name="Akash Hewavickrama" role="Developer" email="chandrasekara.20222237@iit.ac.lk" linkedin="https://www.linkedin.com/in/rajeev-hewavickrama-5297bb266/" github="https://github.com/RajeevAkash" />
-        </div>
-      </div>
-        
-      
-      </div>
-  
     </div>
-  )
-}
+  );
+};
 
-export default MeetTheTeam
+export default MeetTheTeam;
