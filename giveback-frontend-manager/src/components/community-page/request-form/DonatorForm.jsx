6c69a08564d8c1b85f5ac5cdbf240ea8cdd429d2
Pramodh Lihinikaduwa <118333512+pramodh123kit@users.noreply.github.com>
2024-03-20 20:21:04 +0530

    Revert "Created a Popup card for Organization Form and Donation Form"

--- giveback-frontend-manager/src/components/community-page/request-form/DonatorForm.jsx
+++ giveback-frontend-manager/src/components/community-page/request-form/DonatorForm.jsx
@@ -3,13 +3,10 @@ import axios from 'axios';
 import styles from '@/components/community-page/request-form/form.module.css';
 import close_icon from '@/assets/close_iconn.svg';
 import Cookies from 'universal-cookie';
-import SuccessPopup from './SuccessfullyAdded';
 
 const cookies = new Cookies();
 
 const DonatorForm = ({ onClose }) => {
-    const [showSubmit, setShowSubmit] = useState(false);
-    const [showSuccessPopup, setShowSuccessPopup] = useState(false);
     const [itemType, setItemType] = useState('');
     const [itemName, setItemName] = useState('');
     const [itemDescription, setItemDescription] = useState('');
@@ -24,6 +21,7 @@ const DonatorForm = ({ onClose }) => {
   
     const handleSubmit = async (e) => {
       e.preventDefault();
+  
       try {
         const formData = new FormData();
         formData.append('image', image);
@@ -63,9 +61,6 @@ const DonatorForm = ({ onClose }) => {
         // Save the updated donation history back to localStorage
         localStorage.setItem(`donationHistory_${userId}`, JSON.stringify(updatedDonationHistory));  
   
-
-        setShowSubmit(false);
-        setShowSuccessPopup(true);
         setSuccessMessage('Donation submitted successfully!');
       } catch (error) {
         console.error('Error submitting form:', error);
@@ -79,10 +74,7 @@ const DonatorForm = ({ onClose }) => {
     
     return (
       <div className={styles.modalOverlay}>
-            {showSuccessPopup ? ( 
-                <SuccessPopup onClose={onClose} />
-            ) : (
-                <div className={styles.popupCard}>
+        <div className={styles.popupCard}>
           <img src={close_icon} alt="close" className={styles.close_icon} onClick={onClose} />
           <>
             <h1 className={styles.form_header}>Donator's Form</h1>
@@ -133,13 +125,12 @@ const DonatorForm = ({ onClose }) => {
               )}
               <div className={styles.form_btn}>
                 <input className={styles.next_btn} type="submit" value="submit" />
-                </div>
-                        </form>
-                    </>
-                </div>
-            )}
+              </div>
+            </form>
+          </>
         </div>
+      </div>
     );
-};
+  };
   
   export default DonatorForm;
