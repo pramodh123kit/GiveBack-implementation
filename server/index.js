e5a1d0ae6f0943cc7b096e2f35ec3df1fd996784
Pramodh123kit <pramodh123kit@gmail.com>
2024-03-20 14:10:06 +0530

    Remove the mongoDB atlas connection (testing)

--- server/index.js
+++ server/index.js
@@ -21,10 +21,9 @@ app.use(cors());
 app.use(express.json());
 
 // Connecting to MongoDB
-mongoose.connect('mongodb+srv://pramodh123kit:mbk0rtG4jOr5I3X1@cluster0.k4yd2h3.mongodb.net/Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
+// mongoose.connect('mongodb+srv://pramodh123kit:mbk0rtG4jOr5I3X1@cluster0.k4yd2h3.mongodb.net/Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/donationDB');
+
 
 app.use('/donations', donationRoutes);
 app.use('/recipients', recipientRoutes);
