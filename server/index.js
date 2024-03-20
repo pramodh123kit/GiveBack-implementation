39ab483f9026a3ade0e8c232cb42ac5f491a5e01
Pramodh123kit <pramodh123kit@gmail.com>
2024-03-20 19:18:53 +0530

    Added the web.config file to the root directory

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connecting to MongoDB
-mongoose.connect('mongodb+srv://pramodh123kit:mbk0rtG4jOr5I3X1@cluster0.k4yd2h3.mongodb.net/Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect('mongodb://localhost:27017/donationDB');
+// mongoose.connect('mongodb+srv://pramodh123kit:mbk0rtG4jOr5I3X1@cluster0.k4yd2h3.mongodb.net/Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/donationDB');
 // app.use(express.static(path.join(__dirname, 'giveback-frontend-manager', 'build')));
 
 app.use('/donations', donationRoutes);
